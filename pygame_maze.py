8e9969e13883582a5bb61771bfeb129c12436f8b
Abbey <abbeymw@umich.com>
2016-11-26 21:56:36 -0500

    finished maze

--- pygame_maze.py
+++ pygame_maze.py
@@ -63,8 +63,11 @@ class Environ(Setting):
                  [780, 350, 20, 350, WHITE],
                  [20, 0, 760, 20, WHITE],
                  [20, 580, 760, 20, WHITE],
-                 [300, 20, 20, 500, BLUE],
-                 [300, 500, 200, 20, BLUE],
+                 [300, 20, 20, 450, BLUE],
+                 [300, 470, 200, 20, BLUE],
+                 [320, 535, 20, 45, BLUE],
+                 [480, 535, 20, 45, BLUE],
+                 [400, 480, 20, 60, BLUE],
                  [100, 200, 20, 100, BLUE], 
                  [100, 200, 200, 20, BLUE],
                  [370, 200, 20, 200, BLUE],
@@ -80,7 +83,41 @@ class Environ(Setting):
                  [120, 50, 80, 20, BLUE],
                  [200, 50, 20, 50, BLUE],
                  [170, 100, 50, 20, BLUE],
-                 [550, 20, 20, 130, BLUE]
+                 [550, 20, 20, 130, BLUE],
+                 [550, 130, 50, 20, BLUE],
+                 [560, 400, 20, 180, BLUE],
+                 [440, 400, 120, 20, BLUE],
+                 [450, 250, 20, 110, BLUE],
+                 [450, 340, 90, 20, BLUE],
+                 [515, 200, 20, 95, BLUE],
+                 [570, 200, 20, 115, BLUE],
+                 [610, 50, 110, 20, BLUE],
+                 [720, 50, 20, 300, BLUE],
+                 [740, 145, 40, 20, BLUE],
+                 [650, 110, 20, 205, BLUE],
+                 [590, 245, 60, 20, BLUE],
+                 [20, 230, 35, 20, BLUE],
+                 [610, 350, 130, 20, BLUE],
+                 [630, 370, 20, 180, BLUE],
+                 [650, 530, 60, 20, BLUE],
+                 [710, 490, 20, 60, BLUE],
+                 [685, 470, 45, 20, BLUE],
+                 [685, 405, 95, 20, BLUE],
+                 [20, 520, 70, 20, BLUE],
+                 [125, 405, 20, 175, BLUE],
+                 [180, 480, 20, 100, BLUE],
+                 [180, 525, 100, 20, BLUE],
+                 [150, 220, 20, 130, BLUE],
+                 [150, 350, 110, 20, BLUE],
+                 [255, 260, 20, 110, BLUE],
+                 [200, 255, 75, 20, BLUE],
+                 [200, 255, 20, 70, BLUE],
+                 [180, 460, 60, 20, BLUE],
+                 [240, 410, 20, 70, BLUE],
+                 [180, 410, 70, 20, BLUE],
+                 [20, 330, 95, 20, BLUE],
+                 [60, 380, 20, 105, BLUE],
+                 [80, 440, 60, 20, BLUE]
                 ]
         for w in walls_creation:
             wall = Wall(w[0], w[1], w[2], w[3], w[4])
@@ -104,22 +141,22 @@ def main():
  
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    player.change_speed(-5, 0)
+                    player.change_speed(-3, 0)
                 if event.key == pygame.K_RIGHT:
-                    player.change_speed(5, 0)
+                    player.change_speed(3, 0)
                 if event.key == pygame.K_UP:
-                    player.change_speed(0, -5)
+                    player.change_speed(0, -3)
                 if event.key == pygame.K_DOWN:
-                    player.change_speed(0, 5)
+                    player.change_speed(0, 3)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    player.change_speed(5, 0)
+                    player.change_speed(3, 0)
                 if event.key == pygame.K_RIGHT:
-                    player.change_speed(-5, 0)
+                    player.change_speed(-3, 0)
                 if event.key == pygame.K_UP:
-                    player.change_speed(0, 5)
+                    player.change_speed(0, 3)
                 if event.key == pygame.K_DOWN:
-                    player.change_speed(0, -5)
+                    player.change_speed(0, -3)
 
         player.move(room.wall_list)
  

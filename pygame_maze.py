4e242073ad43abc9493539617e755adb3e95f6df
Abbey <abbeymw@umich.com>
2016-11-28 23:04:56 -0500

    progress on enemy sprites

--- pygame_maze.py
+++ pygame_maze.py
@@ -4,47 +4,58 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
  
-
- 
 class player_(pygame.sprite.Sprite):
-    speed_x = 0
-    speed_y = 0
+    x_pos = 0
+    y_pos = 0
  
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.Surface([25, 25])
         self.image.fill((255, 0, 255))
         self.rect = self.image.get_rect()
-        self.rect.y = y
         self.rect.x = x
+        self.rect.y = y
  
-    def change_speed(self, x, y):
-        self.speed_x += x
-        self.speed_y += y
+    # def change_speed(self, x, y):
+    #     self.speed_x += x
+    #     self.speed_y += y
  
-    def move(self, walls):
-        self.rect.x += self.speed_x
-        list_of_blocks = pygame.sprite.spritecollide(self, walls, False)
-        for b in list_of_blocks:
-            if self.speed_x > 0:
-                self.rect.right = b.rect.left
-            else:
-                self.rect.left = b.rect.right
-        self.rect.y += self.speed_y
-        list_of_blocks = pygame.sprite.spritecollide(self, walls, False)
-        for b in list_of_blocks:
-            if self.speed_y > 0:
-                self.rect.bottom = b.rect.top
-            else:
-                self.rect.top = b.rect.bottom
+    # def move(self, walls):
+    #     self.rect.x += self.x_pos
+    #     list_of_blocks = pygame.sprite.spritecollide(self, walls, False)
+    #     for b in list_of_blocks:
+    #         if self.x_pos > 0:
+    #             self.rect.right = b.rect.left
+    #         else:
+    #             self.rect.left = b.rect.right
+    #     self.rect.y += self.y_pos
+    #     list_of_blocks = pygame.sprite.spritecollide(self, walls, False)
+    #     for b in list_of_blocks:
+    #         if self.y_pos > 0:
+    #             self.rect.bottom = b.rect.top
+    #         else:
+    #             self.rect.top = b.rect.bottom
+    def handle_keys(self, walls):
+        """ Handles Keys """
+        key = pygame.key.get_pressed()
+        dist = 2 # distance moved in 1 frame, try changing it to 5
+        if key[pygame.K_DOWN]: # down key
+            self.y_pos += dist # move down
+        elif key[pygame.K_UP]: # up key
+            self.y_pos -= dist # move up
+        if key[pygame.K_RIGHT]: # right key
+            self.x_pos += dist # move right
+        elif key[pygame.K_LEFT]: # left key
+            self.x_pos -= dist
+
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h, color):
         super().__init__()
         self.image = pygame.Surface([w, h])
         self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x 
+        self.rect.x = x
+        self.rect.y = y 
         
 class Setting(object):
     wall_list = None
@@ -53,6 +64,24 @@ class Setting(object):
     def __init__(self):
         self.wall_list = pygame.sprite.Group()
         self.enemy_sprites = pygame.sprite.Group()
+
+# class Enemies(pygame.sprite.Sprite):
+#     def __init__(self, x, y, w, h, color):
+#         super().__init__()
+#         self.image = pygame.Surface([w, h])
+#         self.image.fill(color)
+#         self.rect = self.image.get_rect()
+#         self.rect.x = x
+#         self.rect.y = y 
+#         # self.draw()
+
+# class Enemy_placement(Setting):
+#     def __init__(self):
+#         super().__init__()
+#         enemies_creation = [[20, 20, 20, 20, GREEN]]
+#         for e in enemies_creation:
+#             enemy = Enemies(e[0], e[1], e[2], e[3], e[4])
+#             self.enemy_sprites.add(enemy)
  
 class Maze(Setting):
     def __init__(self):
@@ -63,13 +92,15 @@ class Maze(Setting):
                  [780, 350, 20, 350, WHITE],
                  [20, 0, 760, 20, WHITE],
                  [20, 580, 760, 20, WHITE],
-                 [300, 20, 20, 450, BLUE],
-                 [300, 470, 200, 20, BLUE],
+                 [310, 20, 20, 40, BLUE],
+                 [310, 90, 20, 380,BLUE],
+                 [310, 150, 80, 20, BLUE],
+                 [310, 470, 200, 20, BLUE],
                  [320, 535, 20, 45, BLUE],
                  [480, 535, 20, 45, BLUE],
                  [400, 480, 20, 60, BLUE],
                  [100, 200, 20, 100, BLUE], 
-                 [100, 200, 200, 20, BLUE],
+                 [100, 200, 220, 20, BLUE],
                  [370, 200, 20, 200, BLUE],
                  [370, 55, 20, 100, BLUE],
                  [430, 20, 20, 140, BLUE],
@@ -122,33 +153,42 @@ class Maze(Setting):
         for w in walls_creation:
             wall = Wall(w[0], w[1], w[2], w[3], w[4])
             self.wall_list.add(wall)
- 
 
 def main():
+    #game stuff
     pygame.init()
     disp = pygame.display.set_mode([800, 600])
-    pygame.display.set_caption("Abbey's Maze Game")
+    pygame.display.set_caption("Abbey's Maze Game for Project 4")
     player = player_(20, 555)
     movingsprites = pygame.sprite.Group()
     movingsprites.add(player)
     maze = Maze()
+    # enemies = Enemy_placement()
 
+    #time clock
     clock = pygame.time.Clock()
     font = pygame.font.Font(None, 25)
     f_count = 0
     f_rate = 60
     start_time = 60
 
-    done = False
-    while not done: 
-        player.move(maze.wall_list)
+    #music/sound
+    pygame.mixer.init()
+    # pygame.mixer.music.load()
+
+
+
+    game_over = False
+    while not game_over: 
+        player.handle_keys(maze.wall_list)
+        # player.handle_keys()
         disp.fill((0, 0, 0))
 
         tot_secs = start_time - (f_count//f_rate)
-        if tot_secs < 0:
-            done = True
-            print("you ran out of time")
-        secs = tot_secs % 60
+        # if tot_secs < 0:
+        #     game_over = True
+        #     print("You ran out of time!")
+        secs = tot_secs%60
         mins = (tot_secs//60)
         time_shown = "Time: {0:02}:{1:02}".format(mins, secs)
         text = font.render(time_shown, True, WHITE)
@@ -158,37 +198,51 @@ def main():
  
         movingsprites.draw(disp)
         maze.wall_list.draw(disp)
+        # enemies.draw(disp)
+
  
         pygame.display.flip()
 
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.change_speed(-2, 0)
-                if event.key == pygame.K_RIGHT:
-                    player.change_speed(2, 0)
-                if event.key == pygame.K_UP:
-                    player.change_speed(0, -2)
-                if event.key == pygame.K_DOWN:
-                    player.change_speed(0, 2)
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    player.change_speed(2, 0)
-                if event.key == pygame.K_RIGHT:
-                    player.change_speed(-2, 0)
-                if event.key == pygame.K_UP:
-                    player.change_speed(0,2)
-                if event.key == pygame.K_DOWN:
-                    player.change_speed(0, -2)
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_LEFT:
+            #         player.change_speed(-2, 0)
+            #     if event.key == pygame.K_RIGHT:
+            #         player.change_speed(2, 0)
+            #     if event.key == pygame.K_UP:
+            #         player.change_speed(0, -2)
+            #     if event.key == pygame.K_DOWN:
+            #         player.change_speed(0, 2)
+            # if event.type == pygame.KEYUP:
+            #     if event.key == pygame.K_LEFT:
+            #         player.change_speed(2, 0)
+            #     if event.key == pygame.K_RIGHT:
+            #         player.change_speed(-2, 0)
+            #     if event.key == pygame.K_UP:
+            #         player.change_speed(0,2)
+            #     if event.key == pygame.K_DOWN:
+            #         player.change_speed(0, -2)
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_LEFT:
+            #         player.x_pos -= 1
+            #     if event.key == pygame.K_RIGHT:
+            #         player.x_pos += 1
+            #     if event.key == pygame.K_UP:
+            #         player.y_pos -= 1
+            #     if event.key == pygame.K_DOWN:
+            #         player.y_pos += 1
+
 
             if player.rect.x > 801:
-                done = True
+                game_over = True
                 print("You won!")
 
             if event.type == pygame.QUIT:
-                done = True
+                game_over = True
 
     pygame.quit()
  
 if __name__ == "__main__":
     main()
+
+
